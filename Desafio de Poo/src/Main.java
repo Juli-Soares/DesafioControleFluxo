f32041333e3c7c5178396c565f0d626483c4ca88
Juliana <juh.yazbek8@gmail.com>
2024-05-29 20:23:53 -0300

    Second

--- Desafio de Poo/src/Main.java
+++ Desafio de Poo/src/Main.java
@@ -2,19 +2,16 @@ public class Main {
     public static void main(String[] args) {
         Iphone meuIPhone = new Iphone();
 
-        // Usando as funcionalidades do Reprodutor Musical
         meuIPhone.tocarMusica();
         meuIPhone.pausarMusica();
         meuIPhone.proximaMusica();
         meuIPhone.musicaAnterior();
 
-        // Usando as funcionalidades do Aparelho Telefônico
         meuIPhone.fazerChamada("(xx)1234-5678");
         meuIPhone.atenderChamada();
         meuIPhone.encerrarChamada();
         meuIPhone.iniciarCorreioVoz();
 
-        // Usando as funcionalidades do Navegador de Internet
         meuIPhone.exibirPagina("www.exemplo.com");
         meuIPhone.adicionarNovaAba();
         meuIPhone.atualizarPagina();
